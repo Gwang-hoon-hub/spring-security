b901e18ad3987e025e722f8f66e00132d0dcb2e0
panghoon <leegwanghoon95@gmail.com>
2022-05-16 23:07:30 +0900

    [FEAT] : 1. JsonObject use 2. ExceptionFilter add before LogoutFilter in SecurityConfig

--- src/main/java/com/pang/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/pang/springsecurity/config/SecurityConfig.java
@@ -1,10 +1,41 @@
 package com.pang.springsecurity.config;
 
+import com.pang.springsecurity.security.fiilter.ExceptionFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity(debug = true) // 통과하고 있는 필터체인을 보여준다.
 @EnableGlobalMethodSecurity (prePostEnabled = true)    //
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @Autowired
+    private ExceptionFilter filterChainExceptionHandler;    // 예외처리 하는 필터
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(filterChainExceptionHandler, LogoutFilter.class);
+        super.configure(http);
+    }
 }
---
+++ src/main/java/com/pang/springsecurity/utils/JsonObjectMapper.java
@@ -0,0 +1,6 @@
+package com.pang.springsecurity.utils;
+
+import com.google.gson.Gson;
+
+public class JsonObjectMapper {
+}
