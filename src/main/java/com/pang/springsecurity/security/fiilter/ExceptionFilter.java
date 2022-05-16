8b3b3160e3960b1d786467fa8540546fb9383078
panghoon <leegwanghoon95@gmail.com>
2022-05-16 18:04:29 +0900

    [FEAT] : spring security ExceptionFilter

--- src/main/java/com/pang/springsecurity/security/fiilter/ExceptionFilter.java
+++ src/main/java/com/pang/springsecurity/security/fiilter/ExceptionFilter.java
@@ -1,4 +1,38 @@
 package com.pang.springsecurity.security.fiilter;
 
-public class ExceptionFilter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("Spring Security Filter Chain Exception:", e);
+            resolver.resolveException(request, response, null, e);
+        }
+    }
 }
