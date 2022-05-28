7db25905f92364e2047e9805735eff5109750000
panghoon <leegwanghoon95@gmail.com>
2022-05-28 17:31:39 +0900

    [FEAT] :add WebSocket - html, js

--- src/main/java/com/pang/springsecurity/config/WebSocketConfig.java
+++ src/main/java/com/pang/springsecurity/config/WebSocketConfig.java
@@ -1,10 +1,14 @@
 package com.pang.springsecurity.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -18,5 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
--- src/main/java/com/pang/springsecurity/controller/WebSocketController.java
+++ src/main/java/com/pang/springsecurity/controller/WebSocketController.java
@@ -11,7 +11,7 @@ import org.springframework.web.util.HtmlUtils;
 public class WebSocketController {
 
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
+    @SendTo("/topic/greetings") // 여기를 구독하는 사용자에게 Content 객체를 모두 보여주게 된다.
     public Content greeting (HelloMessage message) throws InterruptedException {
         Thread.sleep(1000); // simulated delay
         return new Content("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
